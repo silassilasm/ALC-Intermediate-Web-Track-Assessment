9a7dea38ac114926bbd9b24f460d31d39719f69e
silassilasm <silassilasm@gmail.com>
2017-11-05 23:34:05 +0100

    Create server.js
    server commit

---
+++ server.js
@@ -0,0 +1,15 @@
+//Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+
+//Express
+var app = express();
+app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.json());
+
+//Routes
+app.use('/api',  require('./routes/api'));
+
+//Start server
+app.listen(3000);
+console.log('API is running on port 3000');
